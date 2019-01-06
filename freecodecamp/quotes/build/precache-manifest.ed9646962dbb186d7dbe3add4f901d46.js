0324417bff576d5ad4dd8bf780d2f0c8a8ade683
Marina Gudym <mari23rus@gmail.com>
2019-01-06 19:15:01 +0200

    added folder build for quotes project

---
+++ freecodecamp/quotes/build/precache-manifest.ed9646962dbb186d7dbe3add4f901d46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea2cc2579b94a34cd757",
+    "url": "/static/css/main.959d6ca2.chunk.css"
+  },
+  {
+    "revision": "ea2cc2579b94a34cd757",
+    "url": "/static/js/main.ea2cc257.chunk.js"
+  },
+  {
+    "revision": "61b63230e5b0e76c90bd",
+    "url": "/static/js/1.61b63230.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "79aee4eb9dabfb3291e232eaf160c700",
+    "url": "/static/media/twitter-color.79aee4eb.svg"
+  },
+  {
+    "revision": "6378f9ac518d69edf6558dacbb69365d",
+    "url": "/index.html"
+  }
+];
